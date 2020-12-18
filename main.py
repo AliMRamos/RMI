d5172721cf87004ff123e83d460e30209db3371f
Alicia Muñoz Ramos <munozramosalicia@gmail.com>
2020-12-18 11:30:42 +0100

    Creates .gitignore and main-like file

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+The aim of the simulation is to be able to determine the initial energy of the e- by analizing the trajectory of the scattering using NN
+
+Index to follow:
+    1. Introduction to e-
+    2. Multiple scattering through small angles
+    3. Results obtained by the simulations of the e- scattering in Xenon
+    4. Results obtained by the use of NN recognition over the scattering pattern to determine the energy of the e-
+
+2. Rutherford cross section -> Coulomb scattering from nuclei
+    tetha_0 = tetha_plane = (1/sqrt(2))*tetha_space
+    (dato - media)^2 == varianza
+
+https://pdg.lbl.gov/2020/reviews/rpp2020-rev-passage-particles-matter.pdf
+"""
+
+"""
+energia_inicial_electron
+Electron se produce en el medio
+Recorre 1mm por step ->dx
+Calculo por la integracion 
+
+"""
